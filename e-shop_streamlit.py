906e01d7f9a4612b790f37ec8ab2c75282b6d311
Dimitri JULES <dimitri.jules@isen-ouest.yncrea.fr>
2022-12-13 15:27:33 +0100

    0.2

--- e-shop_streamlit.py
+++ e-shop_streamlit.py
@@ -21,7 +21,7 @@ from sklearn.model_selection import train_test_split
 # Charge le modèle enregistré et test à partir du fichier chargé
 import cloudpickle as cp
 from urllib.request import urlopen
-mydict = cp.load(urlopen("https://github.com/Dimitri-J/Social_Network_Brief/raw/main/Files/fl.pkl")) 
+mydict = cp.load(urlopen("https://github.com/Dimitri-J/Cars_Seller/raw/main/cars.pkl")) 
 
 # mydict = pickle.load(pick)         # load file content as mydict
 # f.close()                       
@@ -43,30 +43,38 @@ print(mydict)
 
 # Déclaration de variable formulaire
 
-age= "age"
-sex= "sex"
-salaire= "salaire"
+# 'marque', 'fuelsystem', 'doornumber', 'carwidth', 'curbweight','enginesize', 'horsepower'
+
+marque= "marque"
+fuelsystem= "fuelsystem"
+doornumber= "doornumber"
+carwidth="carwidth"
+curbweight="curbweight"
+enginesize="enginesize"
+horsepower="horsepower"
 test= []
 y_pred= []
 
 
 # Config page Streamlit
 
-st.set_page_config(page_title="e-Shop Predictor", layout="wide")
+st.set_page_config(page_title="e-Shop Car Predictor", layout="wide")
 st.markdown(
          f"""
          <style>
          .stApp {{
-             background-image: url("https://4kwallpapers.com/images/wallpapers/macos-monterey-stock-purple-dark-mode-layers-5k-3840x2160-5896.jpg");
-             background-attachment: fixed;
-             background-size: cover
+            background-image: url("https://w0.peakpx.com/wallpaper/440/206/HD-wallpaper-black-background-car-cars-vehicles.jpg");
+            background-color: black;
+            background-attachment: local;
+            background-position: center;
+            background-size: 35%;
+            background-repeat: no-repeat;
          }}
          </style>
          """,
          unsafe_allow_html=True
      )
-st.subheader('Bonjour,')
-st.title("E-Shop simulator")
+st.title("E-Shop Car predictor")
 st.write("[Clique-ici >](https://www.youtube.com/watch?v=dQw4w9WgXcQ)")
 
 # Formulaire de dimentions iris
@@ -74,58 +82,61 @@ st.write("[Clique-ici >](https://www.youtube.com/watch?v=dQw4w9WgXcQ)")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.header("Sex")
-    sex = st.radio(label = "Indiquer le sex", options =('Homme', 'Femme'), key=sex)
-    st.write('Le sex est ', sex)
-    if sex=='Homme':
-        sex=1
-    else:
-        sex=0
-
+    st.header("Marque")
+    marque = st.selectbox(label = "Indiquer la marque", options =('Nissan', 'alfa-romero', 'audi', 'bmw', 'buick', 'chevrolet',
+       'dodge', 'honda', 'isuzu', 'jaguar', 'maxda', 'mazda', 'mercury',
+       'mitsubishi', 'nissan', 'peugeot', 'plymouth', 'porcshce',
+       'porsche', 'renault', 'saab', 'subaru', 'toyota', 'toyouta',
+       'vokswagen', 'volkswagen', 'volvo', 'vw'), key=marque)
+    st.write('La marque choisi est ', marque)
+    marque = mydict["le_9"].transform([marque])
+    
 with col2:
-    st.header("Age")
-    age = st.slider("Indiquer l'âge", key=age, min_value = 18, max_value = 130, value = 25)
-    st.write('The current number is ', age, 'ans')
-
+    st.header("Fuelsystem")
+    fuelsystem = st.selectbox(label = "Indiquer le système d'injection", options =('1bbl', '2bbl', '4bbl', 'idi', 'mfi', 'mpfi', 'spdi', 'spfi'), key=fuelsystem)
+    st.write('Le type de motorisation est ', str(fuelsystem))
+    fuelsystem = mydict["le_8"].transform([fuelsystem])
+    
 with col3:
-    st.header("Salaire")
-    salaire = st.number_input('Indiquer le salaire potentiel', key=salaire,value = 10, min_value=10, step = 1)
-    st.write('Le salaire potentiel est de ', salaire, 'k €')
+    st.header("Doors")
+    doornumber = st.selectbox(label = "Indiquer le nombre de portes", options =('four', 'two'), key=doornumber)
+    st.write('Le nombre de porte est ', str(doornumber))
+    doornumber = mydict["le_2"].transform([doornumber])
 
+col4, col5, col6, col7 = st.columns(4)
+    
+with col4:
+    st.header("Largeur véhicule")
+    carwidth = st.slider("Indiquer la largeur de la voiture", key=carwidth, min_value = 55, max_value = 75, value = 65)
+    st.write('La largeur de la voiture fait', str(carwidth))
+
+with col5:
+    st.header("Poid véhicule")
+    curbweight = st.slider("Indiquer le poid de la voiture", key=curbweight, min_value = 1400, max_value = 4100, value = 2000)
+    st.write('La largeur de la voiture fait', str(curbweight))
+
+with col6:
+    st.header("Dimension moteur")
+    enginesize = st.slider("Indiquer la cylindré de la voiture", key=enginesize, min_value = 55, max_value = 350, value = 120)
+    st.write('La largeur de la voiture fait', str(enginesize))
+    print(type)
+
+with col7:
+    st.header("Puissance moteur")
+    horsepower = st.slider("Indiquer la puissance de la voiture", key=horsepower, min_value = 55, max_value = 350, value = 120)
+    st.write('La largeur de la voiture fait', str(horsepower))
 
 if st.button("Estimation d'achat"):
     import time
 
     col_b1, col_b2 = st.columns([2,1])
-    test = np.array([sex, age, salaire])
 
+    test = np.array([marque, fuelsystem, doornumber,carwidth,curbweight,enginesize,horsepower])
     print(test)
     test = test.reshape(1, -1)
     print(test)
-    predic = mydict["model"].predict(mydict["stand"].transform(test))
+    predic = mydict["model"].predict(test)
     print(predic)
-    if predic==1 :
-        st.metric("Estimation d'achat : ", "Oui",)
-        html_string = """
-                <audio autoplay>
-                <source src="https://www.myinstants.com/media/sounds/donate-sound-here-comes-the-money.mp3" type="audio/mp3">
-                </audio>
-                """
+    st.write("Le prix est estimé à ",str(round(float(predic), 2))," $")
 
-        sound = st.empty()
-        sound.markdown(html_string, unsafe_allow_html=True)  # will display a st.audio with the sound you specified in the "src" of the html_string and autoplay it
-        time.sleep(2)  # wait for 2 seconds to finish the playing of the audio
-        sound.empty()  # optionally delete the element afterwards
-    else:
-        st.metric("Estimation d'achat : ", "Non",)
-        html_string = """
-                <audio autoplay>
-                <source src="https://www.myinstants.com/media/sounds/engineer_no01_1.mp3" type="audio/mp3">
-                </audio>
-                """
-
-        sound = st.empty()
-        sound.markdown(html_string, unsafe_allow_html=True)  # will display a st.audio with the sound you specified in the "src" of the html_string and autoplay it
-        time.sleep(2)  # wait for 2 seconds to finish the playing of the audio
-        sound.empty()  # optionally delete the element afterwards
         
